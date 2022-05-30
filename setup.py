e360ab6e117bb3e50a7c9f83f290b4591edc9af8
MxThread <106563212+MxThread@users.noreply.github.com>
2022-05-31 00:45:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,11 +22,11 @@ setup(
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.8.9',
+    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10.4',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.6',
   ],
 )
